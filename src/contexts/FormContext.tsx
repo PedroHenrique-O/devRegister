0daefd7e4515b668540a4b193c0f7087d80f5221
Pedro <pedrokaz.henrique@gmail.com>
2021-11-09 14:36:02 -0300

    initial commit

---
+++ src/contexts/FormContext.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createContext, ReactNode, useContext, useReducer } from "react";
+
+type State = {
+    currentStep: number;
+    name: string;
+    level: 0 | 1;
+    email: string;
+    github: string;
+};
+type Action = {
+    type: FormAction;
+    payload: any;
+};
+
+type ContextType = {
+    state: State;
+    dispatch: (action: Action) => void;
+};
+
+type FormProviderProps = {
+    children: ReactNode;
+};
+
+const initialData: State = {
+    currentStep: 0,
+    name: "",
+    level: 0,
+    email: "",
+    github: "",
+};
+
+const FormContext = createContext<ContextType | undefined>(undefined);
+
+export enum FormAction {
+    setCurrentStep,
+    setName,
+    setLevel,
+    setEmail,
+    setGithub,
+}
+const formReducer = (state: State, action: Action) => {
+    switch (action.type) {
+        case FormAction.setCurrentStep:
+            return { ...state, currentStep: action.payload };
+        case FormAction.setName:
+            return { ...state, name: action.payload };
+
+        case FormAction.setLevel:
+            return { ...state, level: action.payload };
+        case FormAction.setEmail:
+            return { ...state, email: action.payload };
+        case FormAction.setGithub:
+            return { ...state, email: action.payload };
+        default:
+            return state;
+    }
+};
+
+//provider methods
+
+export const FormProvider = ({ children }: FormProviderProps) => {
+    const [state, dispatch] = useReducer(formReducer, initialData);
+    const value = { state, dispatch };
+
+    return (
+        <FormContext.Provider value={value}>{children}</FormContext.Provider>
+    );
+};
+
+//Context hook methods
+
+export const useForm = () => {
+    const context = useContext(FormContext);
+    if (context === undefined) {
+        throw new Error("useForm precisa ser usado dentro do FormProvider");
+    }
+    return context;
+};
---
+++ src/pages/FormStep3/index.tsx
@@ -0,0 +1,10 @@
+import { Theme } from "../../components/Theme";
+import * as C from "./styles";
+
+export const FormStep3 = () => {
+    return (
+        <Theme>
+            <C.Container>...Passo 3</C.Container>
+        </Theme>
+    );
+};
